b000446c223a34927ae4d54b0c61ad841d3cbc99
David Pucheta <davidpucheta@gmail.com>
2024-08-12 17:44:36 -0600

    Add SqsPublisher

---
+++ 3.Sqs/SqsPublisher/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+
+var SqsClient = new AmazonSQSClient();  
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    FullName = "David Pucheta",
+    Email = "davidpucheta@gmail.com",
+    DateOfBirth = new DateTime(1978,10,21),
+    GitHubUsername = "davidpucheta"
+};
+
+var queueUrlResponse = await SqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        { "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            } 
+        }
+    }  
+};
+
+var response = await SqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine(response.ToString());
+
